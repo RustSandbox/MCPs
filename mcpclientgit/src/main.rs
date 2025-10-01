346ab7a62311e633075c7256b5e52e8c80173274
Hamze Ghalebi <ghalebi@gmail.com>
2025-10-01 16:57:14 +0200

    add out come of example as comment

--- mcpclientgit/src/main.rs
+++ mcpclientgit/src/main.rs
@@ -8,12 +8,12 @@ use rig::client::ProviderClient;
 use rig::completion::Prompt;
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    eprintln!("Ex-01: Git");
 
     let bedrock_client = rig_bedrock::client::Client::from_env();
     let agent = bedrock_client.agent(ANTHROPIC_CLAUDE_3_HAIKU).preamble("You are git expert");
     let git_command = Command::new(
-        "/Users/hamzeghalebi/projects/tools/github-mcp-server/github-mcp-server"
+        "/Users/hamzeghalebi/projects/tools/github-mcp-server/github-mcp-server" // you should change this to your local path og github mcp server binary file
     ).configure(|cmd| {
         cmd.arg("stdio");
         cmd.arg("all");
@@ -45,3 +45,19 @@ async fn main() -> Result<()> {
 
     Ok(())
 }
+/*
+out come:
+Ex-01: Git
+time=2025-10-01T16:55:41.099+02:00 level=INFO msg="starting server" version=version host="" dynamicToolsets=false readOnly=false
+GitHub MCP Server running on stdio
+service is RunningService { service: (), peer: PeerSink { tx: Sender { chan: Tx { inner: Chan { tx: Tx { block_tail: 0x15400dc00, tail_position: 0 }, semaphore: Semaphore { semaphore: Semaphore { permits: 1024 }, bound: 1024 }, rx_waker: AtomicWaker, tx_count: 2, rx_fields: "..." } } }, is_client: true }, handle: JoinHandle { id: Id(17) }, cancellation_token: CancellationToken { is_cancelled: false }, dg: DropGuard { inner: Some(CancellationToken { is_cancelled: false }) } }
+
+
+
+
+service_info is Some(InitializeResult { protocol_version: ProtocolVersion("2025-03-26"), capabilities: ServerCapabilities { experimental: None, logging: Some({}), completions: None, prompts: Some(PromptsCapability { list_changed: None }), resources: Some(ResourcesCapability { subscribe: Some(true), list_changed: Some(true) }), tools: Some(ToolsCapability { list_changed: Some(true) }) }, server_info: Implementation { name: "github-mcp-server", title: None, version: "version", icons: None, website_url: None }, instructions: None })
+Starting task execution...
+Task completed! Reply: I first searched for repositories owned by the user 'hghalebi' and got a list of 88 repositories. I then chose to create a test issue in the repository named 'PropagandaTracker' with the title 'Test Issue from Rust MCP Client' and the body 'This is a funny test issue created automatically. Please ignore!'. The new issue was created successfully and the URL of the issue is returned.
+
+
+ */
